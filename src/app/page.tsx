6d912e7aed6e97391c78aaae3826a0170de25502
Singh, Jaideep <jaideep.singh@dell.com>
2026-01-17 19:45:47 +0530

    02 customisation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-rose-500">
-      <p>Hello World</p>;
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am a button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am input" />
+        </div>
+        <div>
+          <Progress value={50} max={100} />
+        </div>
+        <div>
+          <Textarea placeholder="I am textarea" />
+        </div>
+        <div>
+          <Checkbox name="a checkbox"/>
+        </div>
+      </div>
     </div>
   );
 }
